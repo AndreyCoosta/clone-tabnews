9ba98ed466a2bcc43719fe7584b271a766f12f0f
AndreyCoosta <98717615+AndreyCoosta@users.noreply.github.com>
2025-01-07 18:29:42 +0000

    Adiciona `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
 function Home() {
-    return <h1>Boo👻</h1>
+  return <h1>Boo👻</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
 export default Home;
